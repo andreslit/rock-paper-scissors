d1cdddcd47837379756efedf8f6f4ddb9018edf6
Andrés Litwiller <17816706+andreslit@users.noreply.github.com>
2023-03-11 00:08:29 -0300

    Finish 5 round game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,14 +22,61 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
     
+    if (!["rock", "paper", "scissors"].includes(playerSelection)) {
+        console.log("Input has to be rock, paper or scissors")
+        return
+    }
+
+    let choiceMap = new Map();
+    choiceMap.set("rock", ["scissors", "paper"]);
+    choiceMap.set("paper", ["rock", "scissors"]);
+    choiceMap.set("scissors", ["paper", "rock"]);
+
     if (playerSelection == computerSelection) {
         return "Draw"
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! Rock beats Scissors"
+    else if (computerSelection == choiceMap.get(playerSelection)[0]) {
+        return `You win! ${playerSelection} beats ${computerSelection}`
     }
-    //TODO complete with the other possibilities
+    else {
+        return `You loose! ${computerSelection} beats ${playerSelection}`
+    }
     return
 }
 
-console.log(playRound("Rock", getComputerChoice()))
+function game() {
+
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        roundResult = playRound(prompt(), getComputerChoice())
+
+        console.log(roundResult)
+
+        switch (roundResult.slice(4, 7)) {
+            case "win":
+                playerScore++;
+                break
+            case "loo":
+                computerScore++;
+                break
+        }
+
+    }
+
+    difference = playerScore - computerScore
+
+    if (difference > 0) {
+        return "You win!"
+    }
+    else if (difference < 0) {
+        return "You loose"
+    }
+    else {
+        return "Draw"
+    }
+
+}
+
+console.log(game())
